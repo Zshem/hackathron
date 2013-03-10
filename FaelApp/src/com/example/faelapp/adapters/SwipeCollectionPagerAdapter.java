65ae0e94b1d3c48143b8265600f37eeaa0013e3d
Marko Pipal <mp42705@gmail.com>
2013-03-10 06:39:36 +0100

    ZBar QRcode scanner works and toasts it's results.

--- FaelApp/src/com/example/faelapp/adapters/SwipeCollectionPagerAdapter.java
+++ FaelApp/src/com/example/faelapp/adapters/SwipeCollectionPagerAdapter.java
@@ -1,28 +1,28 @@
 package com.example.faelapp.adapters;
 
-import com.example.faelapp.fragments.CreditCardFragment;
-import com.example.faelapp.fragments.PayFragment;
-import com.example.faelapp.fragments.SwipeItemFragments;
-
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import com.example.faelapp.fragments.CouponsFragment;
+import com.example.faelapp.fragments.CreditCardFragment;
+import com.example.faelapp.fragments.PayFragment;
+import com.example.faelapp.fragments.SwipeItemFragments;
+
 public class SwipeCollectionPagerAdapter extends FragmentStatePagerAdapter  {
-	
+
 	String[] swipeNames = { "Pay", "Cards", "Coupons", "Transactions" };
-	
+
 //	string[0] =	"Pay";
 //	string[1] = "Cards";
 //	string[2] = "Cuopons";
 //	string[3] = "Transactions";
-//	
-	
+//
+
 	public SwipeCollectionPagerAdapter(FragmentManager fm) {
 	    super(fm);
 	}
-	
+
 	@Override
 	public Fragment getItem(int i) {
 		//Fragment fragment = null;
@@ -31,22 +31,24 @@ public class SwipeCollectionPagerAdapter extends FragmentStatePagerAdapter  {
 			return new PayFragment();
 		case 1:
 			return new CreditCardFragment();
+		case 2:
+			return new CouponsFragment();
 		default:
 			return new SwipeItemFragments();
 		}
-		
+
 	}
-	
+
 	@Override
 	public int getCount() {
 	    return swipeNames.length;
 	}
-	
+
 	@Override
 	public CharSequence getPageTitle(int position) {
 	    return swipeNames[position];
 	}
-	
-	
+
+
 
 }
--- FaelApp/src/com/example/faelapp/fragments/PayFragment.java
+++ FaelApp/src/com/example/faelapp/fragments/PayFragment.java
@@ -1,38 +1,56 @@
 package com.example.faelapp.fragments;
 
 
-import java.util.ArrayList;
-
-import com.dm.zbar.android.scanner.ZBarScannerActivity;
-
-import com.example.faelapp.R;
-import com.example.faelapp.adapters.CreditCardsAdapter;
-import com.example.faelapp.model.CreditCard;
-
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.dm.zbar.android.scanner.ZBarConstants;
+import com.dm.zbar.android.scanner.ZBarScannerActivity;
+import com.example.faelapp.R;
 
 public class PayFragment extends Fragment {
-	
 
-	
+	private static int ZBAR_SCANNER_REQUEST = 1;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		
+		View view = inflater.inflate(R.layout.fragment_pay, null);
+		Button scan = (Button) view.findViewById(R.id.button_pay);
 
-		return inflater.inflate(R.layout.fragment_pay, null);
-		
-		//Intent intent = new Intent(this, ZBarScannerActivity.class);
-		//startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
+		scan.setOnClickListener(new PayOnClickListener(getActivity()));
+		return view;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int returnCode, Intent data) {
+		if(requestCode == ZBAR_SCANNER_REQUEST && returnCode == Activity.RESULT_OK) {
+			Toast.makeText(getActivity(), "Scan result = " + data.getStringExtra(ZBarConstants.SCAN_RESULT), Toast.LENGTH_SHORT).show();
+			Toast.makeText(getActivity(), "Scan result type = " + data.getStringExtra(ZBarConstants.SCAN_RESULT_TYPE), Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	class PayOnClickListener implements OnClickListener {
+
+		private final Context context;
+		PayOnClickListener(Context context) {
+			this.context = context;
+		}
+		@Override
+		public void onClick(View arg0) {
+			Intent intent = new Intent(context, ZBarScannerActivity.class);
+			startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
+		}
 	}
 
 }
