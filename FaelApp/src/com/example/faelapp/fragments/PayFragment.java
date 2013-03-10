bd0b710cdfa37cd5e456b86deac1d801347b98f8
Marko Pipal <mp42705@gmail.com>
2013-03-10 08:45:31 +0100

    Payment complete

--- FaelApp/src/com/example/faelapp/fragments/PayFragment.java
+++ FaelApp/src/com/example/faelapp/fragments/PayFragment.java
@@ -2,7 +2,10 @@ package com.example.faelapp.fragments;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,6 +14,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dm.zbar.android.scanner.ZBarConstants;
@@ -20,6 +24,7 @@ import com.example.faelapp.R;
 public class PayFragment extends Fragment {
 
 	private static int ZBAR_SCANNER_REQUEST = 1;
+	private AlertDialog dialog;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,8 +40,35 @@ public class PayFragment extends Fragment {
 	@Override
 	public void onActivityResult(int requestCode, int returnCode, Intent data) {
 		if(requestCode == ZBAR_SCANNER_REQUEST && returnCode == Activity.RESULT_OK) {
-			Toast.makeText(getActivity(), "Scan result = " + data.getStringExtra(ZBarConstants.SCAN_RESULT), Toast.LENGTH_SHORT).show();
-			Toast.makeText(getActivity(), "Scan result type = " + data.getStringExtra(ZBarConstants.SCAN_RESULT_TYPE), Toast.LENGTH_SHORT).show();
+			String result = data.getStringExtra(ZBarConstants.SCAN_RESULT);
+
+			String[] split = result.split(";");
+
+			View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_view, null);
+			TextView store = (TextView)view.findViewById(R.id.dialog_store);
+			store.setText(split[0].toString());
+			TextView price = (TextView)view.findViewById(R.id.dialog_price);
+			price.setText(split[1].toString());
+
+			dialog = new AlertDialog.Builder(getActivity()).setView(view).create();
+			dialog.setTitle("Receipt payment");
+			dialog.setButton(Dialog.BUTTON_NEGATIVE, "CANCEL PAYMENT", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface in, int which) {
+					Toast.makeText(getActivity(), "Payment cancelled", Toast.LENGTH_LONG).show();
+				}
+			});
+
+			dialog.setButton(Dialog.BUTTON_POSITIVE, "PAY NOW", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface in, int which) {
+					Toast.makeText(getActivity(), "Your have paid for this receipt.\nYou are now poor.", Toast.LENGTH_LONG).show();
+				}
+			});
+
+			dialog.show();
 		}
 	}
 
